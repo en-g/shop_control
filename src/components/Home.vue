4185222e64c1891ab7d0a241f4733a240f054314
Eng <1820591474@qq.com>
2021-08-15 11:30:25 +0800

    login success

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    methods: {
+      //退出登录，清空token并跳转到登录页
+      logout() {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
